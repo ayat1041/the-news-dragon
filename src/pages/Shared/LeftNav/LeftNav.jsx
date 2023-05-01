dcdd16fac56342f4e22bcd02f7d0378a8bc37cbc
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-01 13:58:59 +0600

    moment js

---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,10 @@
+
+const LeftNav = () => {
+    return (
+        <div>
+            <h2>Left Nav</h2>
+        </div>
+    );
+};
+
+export default LeftNav;
---
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -0,0 +1,9 @@
+const RightNav = () => {
+    return (
+        <div>
+            <h2>Right nav</h2>
+        </div>
+    );
+};
+
+export default RightNav;
